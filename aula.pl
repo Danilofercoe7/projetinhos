3fbde4e97acdec02cbd7cf9a4d9d70cdc6a8c10e
Danilofercoe7 <danilofercoe0102@gmail.com>
2025-10-17 15:47:24 -0300

    app aluno

--- aula.pl
+++ aula.pl
@@ -2,7 +2,8 @@
 pip freeze > requirements.txt
 pip install django
 pip install python-dotenv 
-python -m venv venv  
+python -m venv venv 
+python manage.py runserver 
 python manage.py runserver
 django-admin help
 django-admin startproject setup .
@@ -10,9 +11,12 @@ git init
 git add .dsv
 git commit -m "alteracoes"
 git push origin master
-git commit -m "primeiro commit"
 git config --global user.email ""
 git config --global user.name ""
 git remote add origin
 git push -u origin master 
 git clone 
+git add .
+git commit -m "app aluno criado"
+git push origin master
+
